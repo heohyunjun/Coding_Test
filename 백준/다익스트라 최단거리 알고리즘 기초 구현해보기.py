523750eb92ae19268a1a5e4fb5ffaf35cb0aa4c0
heohyunjun <hhj2676@naver.com>
2022-03-07 23:51:30 +0900

    feat : 다익스트라 알고리즘 구현해보기

--- 백준/다익스트라 최단거리 알고리즘 기초 구현해보기.py
+++ 백준/다익스트라 최단거리 알고리즘 기초 구현해보기.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-
-# 다익스트라 알고리즘
-"""
-
 # 노드 개수, 간선 개수 입력 받기
 n, m = map(int, input().split())
 
@@ -17,10 +11,23 @@ graph = [[] for i in range(n + 1)]
 # 방문 여부 체크하는 리스트
 visited = [False] * (n + 1)
 
-
 # 최단 거리 테이블 모드 무한으로 초기화
 distance = [INF] * (n + 1)
 
 # 방문 하지 않은 노드 중에서, 가장 최단 거리가 짧은 노드 번호 반환하는 함수
 def get_smallest_node():
-    pass
+    min_value = INF
+
+    # 가장 최단 거러기 짧은 노드
+    index = 0
+    for i in range(1, n+ 1):
+        if distance[i] < min_value and not visited[i]:
+            min_value = distance[i]
+            index = i
+    return index
+
+def dijkstra(start):
+    distance[start] = True
+    visited[start] = True
+    for j
+    ## # #
