8de0195ebb530ee2b0e106b668757c5155e6de16
heohyunjun <hhj2676@naver.com>
2022-03-22 23:51:20 +0900

    feat : programmers sol

--- 프로그래머스/Level 2/구명보트.py
+++ 프로그래머스/Level 2/구명보트.py
@@ -6,3 +6,19 @@
     2번쨰 4번째 사람은 같이 탈 수 있지만, 1번째 3번째 사람 무게합은 130이기 떄문에 불가능
 3. 구명 보트를 최대한 적게 사용하여 모든 사람을 구출하려고 함
 '''
+def solution(people, limit):
+    answer = 0
+    start = 0
+    end = len(people) - 1
+
+    # 오름 차순 정렬
+    people.sort()
+    while start <= end:
+        answer +=1
+
+        # 작은놈, 큰놈 둘 다 태울수 있는 경우
+        if people[start] + people[end] <= limit:
+            start +=1
+        # 둘다 못태우면 큰놈만 태움
+        end -=1
+    return answer
