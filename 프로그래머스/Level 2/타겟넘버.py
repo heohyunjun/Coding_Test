b8819cc963e03a8ea9a4197771a4f482780ce1bb
heohyunjun <hhj2676@naver.com>
2022-04-09 20:11:41 +0900

    feat : programmers sol

--- 프로그래머스/Level 2/타겟넘버.py
+++ 프로그래머스/Level 2/타겟넘버.py
@@ -21,12 +21,36 @@ def solution(numbers, target):
         base_ = tmp
     return base_.count(target)
 
-# dfs로도 풀어보기
+# bfs로도 풀어보기
 # 각 노드에 인덱스를 부여해서 더하기
-from collections import deque
 
+'''
+아이디어
+방문한 원소값과 그에 해당하는 index값을 같이 큐에 추가해서 bfs방식으로 해결
+'''
+from collections import deque
 def solution(numbers, target):
-    n = len(numbers)
     answer = 0
-    def dfs(idx, result):
-        pass
+    queue = deque()
+    n = len(numbers)
+
+    # 처음 경우의수 ([1, 0] , [-1 , 0] )
+    # +1의 인덱스 0, -1 과 인덱스 0
+    queue.append([numbers[0],0])
+    queue.append([-1*numbers[0],0])
+    while queue:
+        temp, idx = queue.popleft()
+
+        # 계산을 하면 다음 노드로 넘어가니까, 인덱스 +1
+        idx += 1
+
+        # 총 5번 계산만 해야함
+        if idx < n:
+            # +1 ,-1을 각각해주고 인덱스와 같이 다시 큐에 추가한다
+            queue.append([temp+numbers[idx], idx])
+            queue.append([temp-numbers[idx], idx])
+        # target값이랑 같으면 +1
+        else:
+            if temp == target:
+                answer += 1
+    return answer
