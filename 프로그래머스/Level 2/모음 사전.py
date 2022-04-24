a900be70afe84e5d2eb9547d4c0362b75531a552
heohyunjun <hhj2676@naver.com>
2022-04-24 23:10:58 +0900

    feat : programmers sol

--- 프로그래머스/Level 2/모음 사전.py
+++ 프로그래머스/Level 2/모음 사전.py
@@ -7,4 +7,18 @@ title : 모음 사전
 word 길이는 1이상 5이하
 
 단어 하나 word가 매개변수로 주어질 때, 사전에서 몇 번쨰 단어인지 리턴
+
+아이디어 : 단순하게 사전 다 만들어서 인덱스 찾음
 '''
+
+# from itertools import product
+def solution(word):
+
+    dict_ = []
+    for i in range(1,6) :
+        dict_ += list(map(''.join, product("AEIOU", repeat = i)))
+    dict_.sort()
+
+    answer = dict_.index(word) + 1
+
+    return answer
