2fd481813a7312f47fd4e4c42c0e2a7895593d57
heohyunjun <hhj2676@naver.com>
2022-05-08 13:13:05 +0900

    feat : programmers sol 1. 완전탐색하여 해결

--- 프로그래머스/Level 2/문자열 압축.py
+++ 프로그래머스/Level 2/문자열 압축.py
@@ -10,3 +10,20 @@ ex) aabbaccc -> 2a2ba3c 의  방식으로 처음했지만 압축률이 적음
 압축하여 표현한 문자열 중 가장 짧은 것의 길이를 return
 
 '''
+def solution(s):
+    answer = len(s)
+    for i in range(1, len(s)//2 + 1):
+        compressed = ''
+        prev = s[0:i]
+        count = 1
+        for j in range(i, len(s), i):
+            if prev==s[j:j+i]:
+                count+=1
+            else:
+                compressed+=str(count) + prev if count >= 2 else prev
+                prev = s[j:j+i]
+                count = 1
+        compressed+=str(count)+prev if count>=2 else prev
+        answer = min(answer, len(compressed))
+
+    return answer
