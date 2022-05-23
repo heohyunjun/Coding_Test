c9a7f7eab638af4608dcd39d2d97917b9da9416f
heohyunjun <hhj2676@naver.com>
2022-05-23 23:44:58 +0900

    feat : programming sol

--- 프로그래머스/Level 2/[카카오 인턴]수식 최대화.py
+++ 프로그래머스/Level 2/[카카오 인턴]수식 최대화.py
@@ -6,13 +6,41 @@
 참가자에게 주어진 연산 수식이 담긴 문자열 expression이 매개변수로 주어질 때, 우승 시 받을 수 있는 가장 큰 상금 금액을 return
 '''
 
+from itertools import permutations
+
+def calc(op, seq, exp):
+    if exp.isdigit():
+        return str(exp)
+    else:
+        if op[seq] == "*":
+            split_data = exp.split("*")
+            temp = []
+            for s in split_data:
+                temp.append(calc(op, seq + 1, s))
+            return str(eval("*".join(temp)))
+
+        if op[seq] == "+":
+            split_data = exp.split("+")
+            temp = []
+            for s in split_data:
+                temp.append(calc(op, seq + 1, s))
+            return str(eval("+".join(temp)))
+
+        if op[seq] == "-":
+            split_data = exp.split("-")
+            temp = []
+            for s in split_data:
+                temp.append(calc(op, seq + 1, s))
+            return str(eval("-".join(temp)))
+
 def solution(expression):
     answer = 0
     operations = list(permutations(['*', '+', '-'], 3))
-    return answer
 
-def calc(op, seq, exp):
-    if exp.isdigit():  # 더이상 exp에 연산자가 없으면
-        return str(exp)  # 그대로 리턴
-    else:
-        pass
+    for op in operations:
+        result = abs(int(calc(op, 0, expression)))
+
+        if result > answer:
+            answer = result
+
+    return answer
