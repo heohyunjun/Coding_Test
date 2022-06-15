1b26bd2f9806687c4206765b3c9d9ec36043fa3d
heohyunjun <hhj2676@naver.com>
2022-06-16 01:01:58 +0900

    feat : programming..sol...

--- 프로그래머스/Level 2/전력망을 둘로 나누기.py
+++ 프로그래머스/Level 2/전력망을 둘로 나누기.py
@@ -7,3 +7,12 @@ n개의 송전탑이 전선을 통해 하나의 트리 형태로 연결되어 �
 전선들 중 하나를 끊어서 송전탑 개수가 가능한 비슷하도록 두 전력망으로 나누었을 때,
 두 전력망이 가지고 있는 송전탑 개수의 차이(절대값)를 return
 '''
+
+
+def solution(n, wires):
+    answer = n
+    graph = [[] for _ in range(n + 1)]
+
+    for v1, v2 in wires:
+        graph[v1].append(v2)
+        graph[v2].append(v1)
