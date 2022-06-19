da8f8fcadb3f0afee0337f6a40b011cb369cc85f
heohyunjun <hhj2676@naver.com>
2022-06-19 15:59:35 +0900

    feat : programming sol

--- 프로그래머스/Level 2/2개 이하로 다른 비트.py
+++ 프로그래머스/Level 2/2개 이하로 다른 비트.py
@@ -3,3 +3,22 @@
 정수들이 담긴 배열 numbers가 매개변수로 주어집니다.
 numbers의 모든 수들에 대하여 각 수의 f 값을 배열에 차례대로 담아 return
 '''
+
+
+def solution(numbers):
+    answer = []
+    for number in numbers:
+        if number % 2 == 0:  # 짝수 일때
+            binary_num = list(bin(number)[2:])
+            binary_num[-1] = "1"
+        else:  # 홀수 일때
+            binary_num = bin(number)[2:]
+            binary_num = "0" + binary_num
+            one_idx = binary_num.rfind("0")
+            binary_num = list(binary_num)
+            binary_num[one_idx] = "1"
+            binary_num[one_idx + 1] = "0"
+        ans_num = int("".join(binary_num), 2)
+        answer.append(ans_num)
+
+    return answer
