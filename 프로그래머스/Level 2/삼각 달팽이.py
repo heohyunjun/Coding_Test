b64c3f8329bee62c0c57de17a77de7c50493f47a
heohyunjun <hhj2676@naver.com>
2022-06-28 23:32:15 +0900

    feat : programming sol

--- 프로그래머스/Level 2/삼각 달팽이.py
+++ 프로그래머스/Level 2/삼각 달팽이.py
@@ -3,6 +3,28 @@
 다음 그림과(그림생략) 같이 밑변의 길이와 높이가 n인 삼각형에서 맨 위 꼭짓점부터 반시계 방향으로 달팽이 채우기를 진행한 후,
 첫 행부터 마지막 행까지 모두 순서대로 합친 새로운 배열을 return
 '''
+
+
 def solution(n):
+    res = [[0] * n for _ in range(n)]
     answer = []
+    x, y = -1, 0
+    num = 1
+
+    for i in range(n):
+        for j in range(i, n):
+            if i % 3 == 0:  # 아래
+                x += 1
+            elif i % 3 == 1:  # 오른쪽
+                y += 1
+            elif i % 3 == 2:  # 위
+                x -= 1
+                y -= 1
+            res[x][y] = num
+            num += 1
+
+    for i in res:
+        for j in i:
+            if j:
+                answer.append(j)
     return answer
