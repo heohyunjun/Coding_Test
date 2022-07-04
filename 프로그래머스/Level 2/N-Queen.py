3dab20a8583bce0a7c04154d86cca07b8de1432f
heohyunjun <hhj2676@naver.com>
2022-07-04 23:34:23 +0900

    feat : programming sol

--- 프로그래머스/Level 2/N-Queen.py
+++ 프로그래머스/Level 2/N-Queen.py
@@ -4,3 +4,24 @@
 체스판의 가로 세로의 세로의 길이 n이 매개변수로 주어질 때,
 n개의 퀸이 조건에 만족 하도록 배치할 수 있는 방법의 수를 return
 '''
+
+def is_possible(x, y, n, graph):
+    for i in range(x):
+        if y == graph[i] or abs(y-graph[i]) == x-i:
+            return False
+    return True
+
+def queen(x, n ,graph):
+    if x ==n: return 1
+    cnt = 0
+
+    for y in range(n):
+        if is_possible(x, y, n, graph):
+            graph[x] = y
+            cnt += queen(x+1, n, graph)
+    return cnt
+
+def solution(n):
+    graph = [0] * n
+    answer = queen(0, n, graph)
+    return answer
