c2be59ad151d6e63fd3671de9df6e22a06efcfc4
heohyunjun <hhj2676@naver.com>
2022-07-26 23:42:02 +0900

    feat : programming sol

--- 프로그래머스/Level 2/후보키.py
+++ 프로그래머스/Level 2/후보키.py
@@ -9,3 +9,27 @@
 릴레이션을 나타내는 문자열 배열 relation이 매개변수로 주어질 때,
 이 릴레이션에서 후보 키의 개수를 return 하도록 solution
 """
+from itertools import combinations
+
+
+def solution(relation):
+    n_row = len(relation)
+    n_col = len(relation[0])
+
+    candidates = []
+    for i in range(1, n_col + 1):
+        candidates.extend(combinations(range(n_col), i))
+
+    final = []
+    for keys in candidates:
+        tmp = [tuple([item[key] for key in keys]) for item in relation]
+        if len(set(tmp)) == n_row:
+            final.append(keys)
+
+    answer = set(final)
+    for i in range(len(final)):
+        for j in range(i + 1, len(final)):
+            if len(final[i]) == len(set(final[i]).intersection(set(final[j]))):
+                answer.discard(final[j])
+
+    return len(answer)
