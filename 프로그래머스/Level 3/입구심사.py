9007cfddc007dc8ade204238d3a6a9824b8a563a
heohyunjun <hhj2676@naver.com>
2022-08-07 11:10:52 +0900

    feat : programming sol

--- 프로그래머스/Level 3/입구심사.py
+++ 프로그래머스/Level 3/입구심사.py
@@ -11,3 +11,22 @@ n명이 임국 심사를위해 줄을 서서 기다리고 있음
 각 심사관이 한 명을 심사하는데 걸리는 시간 : times
 모든 사람이 심사를 받는데 걸리는 시간의 최솟값 return
 '''
+
+def solution(n, times):
+    answer = 0
+    left = min(times)
+    right = max(times) *n
+    while left<=right:
+        mid = (left+right) //2
+        checked = 0
+        for time in times:
+            checked += mid//time
+            if checked >=n:
+                break
+
+        if checked >=n:
+            answer = mid
+            right = mid -1
+        elif checked <n:
+            left = mid+1
+    return answer
