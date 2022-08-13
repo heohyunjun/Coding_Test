f24e48ef8d45d8abb4533b5741bdbc9b1130168e
heohyunjun <hhj2676@naver.com>
2022-08-13 15:37:01 +0900

    feat : programming sol

--- 프로그래머스/Level 2/게임 맵 최단거리.py
+++ 프로그래머스/Level 2/게임 맵 최단거리.py
@@ -11,5 +11,20 @@ maps는 0과 1로만 이루어져 있으며, 0은 벽이 있는 자리, 1은 벽
 '''
 
 # bfs
+from collections import deque
+
 def solution(maps):
-    d = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    n, m = len(maps), len(maps[0])
+    dx, dy = [1, -1, 0, 0], [0, 0, 1, -1]
+    visited = [[0 for _ in range(m)] for _ in range(n)]
+    visited[0][0] = 1
+    queue = deque([(0, 0)])
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx, ny = x+dx[i], y+dy[i]
+            if 0 <= nx < n and 0 <= ny < m:
+                if visited[nx][ny] == 0 and maps[x][y] == 1:
+                    visited[nx][ny] = visited[x][y]+1
+                    queue.append((nx, ny))
+    return visited[-1][-1] if visited[-1][-1]>0 else -1
