6e541a647f26d56b9d011cb332acc79be5373d3f
heohyunjun <hhj2676@naver.com>
2022-08-16 23:05:37 +0900

    feat : programming sol

--- 프로그래머스/Level 3/야근지수.py
+++ 프로그래머스/Level 3/야근지수.py
@@ -4,3 +4,20 @@ Demi는 N시간 동안 야근 피로도를 최소화하도록 일할예정
 1시간동안 작업량 1만큼을 처리가능하다고 할 때, 퇴근까지 남은 N시간과 일에 대한 작업량 works에 대해 야근
 피로도를 최소화한 값을 리턴
 '''
+
+import heapq
+def solution(n, works):
+    answer = 0
+    if sum(works)<=n:
+        return 0
+    answer = 0
+
+    works = [-w for w in works]
+    heapq.heapify(works)
+    while n >0:
+        max_ = heapq.heappop(works)
+        heapq.heappush(works, max_+1)
+        n-=1
+    for i in works:
+        answer+=i**2
+    return answer
