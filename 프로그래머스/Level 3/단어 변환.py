71d5584e418335dfe45e64dcfafde722e086d074
heohyunjun <hhj2676@naver.com>
2022-08-21 15:01:59 +0900

    think : 손코딩

---
+++ 프로그래머스/Level 3/단어 변환.py
@@ -0,0 +1,11 @@
+'''
+두 개의 단어 begin, target과 단어 집합 words가 있음
+begin -> target으로 변하는 과정
+1 한 번에 한 개의 알파벳만 바꿀 수 있음
+2. words에 있는 단어로만 변환 가능
+예시
+begin :"hit",
+target : cog",
+words : ["hot","dot","dog","lot","log","cog"]
+"hit" -> "hot" -> "dot" -> "dog" -> "cog"와 같이 4단계를 거쳐 변환
+'''
