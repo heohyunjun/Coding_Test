6002f35baa0a9d145f8138c9f7993bef55661fd8
heohyunjun <hhj2676@naver.com>
2022-09-04 22:46:33 +0900

    feat : programming sol

--- 프로그래머스/Level 3/단어 변환.py
+++ 프로그래머스/Level 3/단어 변환.py
@@ -9,3 +9,25 @@ target : cog",
 words : ["hot","dot","dog","lot","log","cog"]
 "hit" -> "hot" -> "dot" -> "dog" -> "cog"와 같이 4단계를 거쳐 변환
 '''
+
+
+from collections import deque
+def solution(begin, target, words):
+    if target not in words:
+        return 0
+    q = deque([])
+    q.append([begin, 0])
+    while q:
+        x, cnt = q.popleft()
+        if x==target:
+            return cnt
+        for i in range(len(words)):
+            diff_cnt = 0
+            word = words[i]
+            for j in range(len(word)):
+                if x[j] != word[j]:
+                    diff_cnt +=1
+            if diff_cnt ==1:
+                q.append([word, cnt+1])
+
+    return 0
